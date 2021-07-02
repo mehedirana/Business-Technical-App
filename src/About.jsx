9535f459f096ffd4fdd1cf4835b3fdda41125dac
mehedirana <mdmehedirana@gmail.com>
2021-07-02 15:08:36 +0600

    routing between 4 page done

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
 
     return (
         <div>
-            <h1>Contact</h1>
+            <h1>About</h1>
         </div>
     )
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
-const App =()=> {
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Service from './Service'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+const App = () => {
   return (
-    <div>
-      <h1> Welcome </h1>
-      <text>hi</text>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/service" component={Service} />
+      </Switch>
+    </>
   );
 }
 
