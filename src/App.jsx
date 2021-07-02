cd15fb7a0a5652fe22f71ff69116d126dd226540
mehedirana <mdmehedirana@gmail.com>
2021-07-02 20:36:42 +0600

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import Home from './Home'
 import About from './About'
 import Contact from './Contact'
 import Service from './Service'
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+// import "../node_modules/react-bootstrap-v5/lib/dist/react-bootstrap.min.css"
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import Navbar from "./Navbar";
 const App = () => {
   return (
     <>
+    <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import App from './App.jsx';
 
 ReactDOM.render(
